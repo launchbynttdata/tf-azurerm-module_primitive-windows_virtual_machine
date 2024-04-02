05c5712292eb340099bccefb26d90bc5ddeaedb6
Chris Taylor <132399041+chris11-taylor-nttd@users.noreply.github.com>
2024-04-02 15:07:56 -0500

    Fix azurerm version constraint, adjust TF required_version (#1)

--- versions.tf
+++ versions.tf
@@ -11,12 +11,12 @@
 // limitations under the License.
 
 terraform {
-  required_version = "<= 1.5.5"
+  required_version = ">= 1.5.0, <= 1.5.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.77.0"
+      version = "~> 3.77"
     }
   }
 }
