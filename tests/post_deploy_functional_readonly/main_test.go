a19f086674cc015bfef62576bb45b748719bc1f3
rakesh gorige <rakesh.gorige@resideo.com>
2026-08-13 16:14:39 -0500

    feat!: convert to copier-managed skeleton

--- tests/post_deploy_functional_readonly/main_test.go
+++ tests/post_deploy_functional_readonly/main_test.go
@@ -33,5 +33,5 @@ func TestWindowsVMModule(t *testing.T) {
 		SetTestConfigFileName(infraTFVarFileNameDefault).
 		Build()
 
-	lib.RunSetupTestTeardown(t, *ctx, testimpl.TestComposableComplete)
+	lib.RunNonDestructiveTest(t, *ctx, testimpl.TestComposableComplete)
 }
